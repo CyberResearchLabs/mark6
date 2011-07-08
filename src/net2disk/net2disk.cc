ef344b3b16e9e18f2d4d062986212e924667a716
David Lapsley <del@haystack.mit.edu>
2011-07-08 09:56:07 -0400

    Incorporated net2disk into automake. Added PF_RING into automated build (within src/extern directory).

--- src/net2disk/net2disk.cc
+++ src/net2disk/net2disk.cc
@@ -44,14 +44,9 @@
 #include <monetary.h>
 #include <locale.h>
 
-#include "pfring.h"
+#include <pfring.h>
 
-/* Constants */
-#define ALARM_SLEEP            1
-#define DEFAULT_SNAPLEN        9000
-#define MAX_NUM_THREADS        64
-#define DEFAULT_DEVICE         "eth0"
-#define NUMBER_OF_FILES       	7 
+#include <net2disk.h>
 
 /* Globals */
 int pages_per_buffer;
@@ -63,13 +58,10 @@ pfring  *pd;
 int verbose = 0, num_threads = 1;
 pfring_stat pfringStats;
 pthread_rwlock_t statsLock;
-
-static struct timeval startTime;
+uint32_t thiszone;
 unsigned long long numPkts[MAX_NUM_THREADS] = { 0 }, numBytes[MAX_NUM_THREADS] = { 0 };
 u_int8_t wait_for_packet = 1, do_shutdown = 0;
-
-/* Forward declarations. */
-void writer_task(int fd, u_char* buf, int buf_size);
+struct timeval startTime;
 
 
 /* *************************************** */
@@ -284,7 +276,6 @@ char* proto2str(u_short proto) {
 }
 
 /* ****************************************************** */
-static int32_t thiszone;
 
 /* DAVE */
 unsigned char mybuf[DEFAULT_SNAPLEN];
@@ -547,7 +538,7 @@ setup() {
   int i;
 
   for (i=0; i<NUMBER_OF_FILES; i++) {
-    if (posix_memalign((void*)&bufs[i], page_size, buffer_size) < 0) {
+    if (posix_memalign((void**)&bufs[i], page_size, buffer_size) < 0) {
       perror("Memalign failed.");
       exit(1);
     }
@@ -572,172 +563,3 @@ setup() {
   printf("buffer_size: %d", buffer_size);
 }
 
-/* *************************************** */
-int main(int argc, char* argv[]) {
-  char *device = NULL, c, *string = NULL, buf[32];
-  u_char mac_address[6];
-  int promisc, snaplen = DEFAULT_SNAPLEN, rc;
-  u_int clusterId = 0;
-  int bind_core = -1;
-  packet_direction direction = rx_and_tx_direction;
-  u_int16_t watermark = 0, poll_duration = 0, cpu_percentage = 0, rehash_rss = 0;
-
-  startTime.tv_sec = 0;
-  thiszone = gmt2local(0);
-
-  while((c = getopt(argc,argv,"hi:c:dl:vs:ae:n:w:p:b:rg:" /* "f:" */)) != '?') {
-    if((c == 255) || (c == -1)) break;
-
-    switch(c) {
-    case 'h':
-      printHelp();
-      return(0);
-      break;
-    case 'a':
-      wait_for_packet = 0;
-      break;
-    case 'e':
-      switch(atoi(optarg)) {
-      case rx_and_tx_direction:
-      case rx_only_direction:
-      case tx_only_direction:
-	direction = atoi(optarg);
-	break;
-      }
-      break;
-    case 'c':
-      clusterId = atoi(optarg);
-      break;
-    case 'l':
-      snaplen = atoi(optarg);
-      break;
-    case 'i':
-      device = strdup(optarg);
-      break;
-    case 'n':
-      num_threads = atoi(optarg);
-      break;
-    case 'v':
-      verbose = 1;
-      break;
-    case 's':
-      string = strdup(optarg);
-      break;
-    case 'w':
-      watermark = atoi(optarg);
-      break;
-    case 'b':
-      cpu_percentage = atoi(optarg);
-      break;
-    case 'p':
-      poll_duration = atoi(optarg);
-      break;
-    case 'r':
-      rehash_rss = 1;
-      break;
-    case 'g':
-      bind_core = atoi(optarg);
-      break;
-    }
-  }
-
-  setup();
-
-  /* Start */
-  
-  if(verbose) watermark = 1;
-  if(device == NULL) device = DEFAULT_DEVICE;
-  if(num_threads > MAX_NUM_THREADS) num_threads = MAX_NUM_THREADS;
-
-  /* hardcode: promisc=1, to_ms=500 */
-  promisc = 1;
-
-  if(num_threads > 0)
-    pthread_rwlock_init(&statsLock, NULL);
-
-  if(wait_for_packet && (cpu_percentage > 0)) {
-    if(cpu_percentage > 99) cpu_percentage = 99;
-    pfring_config(cpu_percentage);
-  }
-
-  pd = pfring_open(device, promisc,  snaplen, (num_threads > 1) ? 1 : 0);
-
-  if(pd == NULL) {
-    printf("pfring_open error (pf_ring not loaded or perhaps you use quick mode and have already a socket bound to %s ?)\n",
-	   device);
-    return(-1);
-  } else {
-    u_int32_t version;
-
-    pfring_set_application_name(pd, "pfcount");
-    pfring_version(pd, &version);
-
-    printf("Using PF_RING v.%d.%d.%d\n",
-	   (version & 0xFFFF0000) >> 16,
-	   (version & 0x0000FF00) >> 8,
-	   version & 0x000000FF);
-  }
-
-  if(pfring_get_bound_device_address(pd, mac_address) != 0)
-    printf("pfring_get_bound_device_address() failed\n");
-  else
-    printf("Capturing from %s [%s]\n", device, etheraddr_string(mac_address, buf));
-
-  printf("# Device RX channels: %d\n", pfring_get_num_rx_channels(pd));
-  printf("# Polling threads:    %d\n", num_threads);
-
-  if(clusterId > 0) {
-    rc = pfring_set_cluster(pd, clusterId, cluster_round_robin);
-    printf("pfring_set_cluster returned %d\n", rc);
-  }
-
-  if((rc = pfring_set_direction(pd, direction)) != 0)
-    printf("pfring_set_direction returned [rc=%d][direction=%d]\n", rc, direction);
-
-  if(watermark > 0) {
-    if((rc = pfring_set_poll_watermark(pd, watermark)) != 0)
-      printf("pfring_set_poll_watermark returned [rc=%d][watermark=%d]\n", rc, watermark);
-  }
-
-  if(rehash_rss)
-    pfring_enable_rss_rehash(pd);
-
-  if(poll_duration > 0)
-    pfring_set_poll_duration(pd, poll_duration);
-
-  signal(SIGINT, sigproc);
-  signal(SIGTERM, sigproc);
-  signal(SIGINT, sigproc);
-
-
-  if(!verbose) {
-    signal(SIGALRM, my_sigalarm);
-    alarm(ALARM_SLEEP);
-  }
-
-  pfring_enable_ring(pd);
-
-  if(num_threads > 1) {
-    pthread_t my_thread;
-    long i;
-
-    for(i=1; i<num_threads; i++)
-      pthread_create(&my_thread, NULL, packet_consumer_thread, (void*)i);
-
-    bind_core = -1;
-  }
-
-  if(bind_core >= 0)
-    bind2core(bind_core);
-
-  // if(1) {
-  // pfring_loop(pd, dummyProcesssPacket, (u_char*)NULL);
-  // } else
-  packet_consumer_thread(0);
-
-  alarm(0);
-  sleep(1);
-  pfring_close(pd);
-
-  return(0);
-}
---
+++ src/net2disk/net2disk.h
@@ -0,0 +1,98 @@
+/*
+ *
+ * (C) 2005-11 - Luca Deri <deri@ntop.org>
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * VLAN support courtesy of Vincent Magnin <vincent.magnin@ci.unil.ch>
+ *
+ */
+
+#ifndef _NET2DISK_H__
+#define _NET2DISK_H__
+
+#define _GNU_SOURCE
+#include <signal.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <sys/poll.h>
+#include <netinet/in_systm.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
+#include <net/ethernet.h>     /* the L2 protocols */
+#include <sys/time.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <monetary.h>
+#include <locale.h>
+
+#include "pfring.h"
+
+/* Constants */
+#define ALARM_SLEEP            1
+#define DEFAULT_SNAPLEN        9000
+#define MAX_NUM_THREADS        64
+#define DEFAULT_DEVICE         "eth0"
+#define NUMBER_OF_FILES       	7 
+
+/* Globals */
+extern int pages_per_buffer;
+extern int page_size;
+extern int buffer_size;
+extern u_char* bufs[NUMBER_OF_FILES];
+extern int fds[NUMBER_OF_FILES];
+extern pfring  *pd;
+extern int verbose;
+extern int num_threads;
+extern pfring_stat pfringStats;
+extern pthread_rwlock_t statsLock;
+extern u_int32_t thiszone;
+
+extern struct timeval startTime;
+extern unsigned long long numPkts[MAX_NUM_THREADS];
+extern unsigned long long numBytes[MAX_NUM_THREADS];
+extern u_int8_t wait_for_packet;
+extern u_int8_t do_shutdown;
+
+/* Forward declarations. */
+extern void writer_task(int fd, u_char* buf, int buf_size);
+extern double delta_time (struct timeval * now, struct timeval * before);
+extern void print_stats();
+extern void sigproc(int sig);
+extern void my_sigalarm(int sig);
+extern char* etheraddr_string(const u_char *ep, char *buf);
+extern char* _intoa(unsigned int addr, char* buf, u_short bufLen);
+char* intoa(unsigned int addr);
+extern inline char* in6toa(struct in6_addr addr6);
+extern char* proto2str(u_short proto);
+extern void dummyProcesssPacket(const struct pfring_pkthdr *h, const u_char *p, const u_char *user_bytes);
+extern int32_t gmt2local(time_t t);
+extern void printHelp(void);
+extern int bind2core(u_int core_id);
+extern void dump_buf(const long thread_id, const u_char* buf);
+extern void* packet_consumer_thread(void* _id);
+extern void writer_task(int fd, u_char* buf, int buf_size);
+extern void setup();
+
+#endif // _NET2DISK_H__
