de72cd468323eb5236e0acf14ab5f2b9b1e5b1e0
David <del@haystack.mit.edu>
2011-08-12 15:04:11 -0400

    Solid 12 Gbps

--- src/net2disk/run.sh
+++ src/net2disk/run.sh
@@ -2,20 +2,37 @@
 
 export LD_LIBRARY_PATH=/home/dlapsley/mark6/src/extern/PF_RING-4.7.0/userland/lib
 
-EXEC=./netdisk
+# Globals
+EXEC=./net2disk
 
-# ./net2disk --interfaces eth2 eth3 eth4 eth5 --capture_files /mnt/disk0/1.m6 /mnt/disk1/1.m6 /mnt/disk2/1.m6 /mnt/disk3/1.m6
-# ./net2disk --interfaces eth2 eth3 --capture_files /mnt/disk0/1.m6 
-# ./net2disk --interfaces eth2 --capture_files /mnt/disk0/1.m6 
+# IRQ "MAP"
+ETH2_IRQ=63
+ETH3_IRQ=65
+ETH4_IRQ=66
+ETH5_IRQ=64
 
+RING_BUFFERS=128
 
+# Setup IRQ AFFINITY (echoing a CPU mask to set affinity)
+echo 2 > /proc/irq/${ETH2_IRQ}/smp_affinity
+echo cat /proc/irq/${ETH2_IRQ}/smp_affinity
+
+echo 2 > /proc/irq/${ETH3_IRQ}/smp_affinity
+echo cat /proc/irq/${ETH3_IRQ}/smp_affinity
+
+echo 4 > /proc/irq/${ETH4_IRQ}/smp_affinity
+echo cat /proc/irq/${ETH4_IRQ}/smp_affinity
+
+echo 4 > /proc/irq/${ETH5_IRQ}/smp_affinity
+echo cat /proc/irq/${ETH5_IRQ}/smp_affinity
+
+# Run! 
+# Note that SMP affinities are ordinals (not mask). CPU numbering from 0.
 ${EXEC} \
-	--interfaces eth2 \
-	--capture_files /mnt/disk0/1.m6
-	--smp_affinity 1
-
-${EXEC} \
-	--interfaces eth3 \
-	--capture_files /mnt/disk0/1.m6 /mnt/disk1/1.m6 \
-	--smp_affinity 2
-
+    --interfaces eth2 eth3 eth4 \
+    --capture_files \
+      /mnt/disk0/cap.m6 /mnt/disk1/cap.m6 \
+      /mnt/disk2/cap.m6 \
+    --smp_affinities 1 1 2 \
+    --ring_buffers ${RING_BUFFERS} \
+    --write_blocks ${RING_BUFFERS}
