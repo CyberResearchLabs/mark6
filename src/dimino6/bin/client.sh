6c6bd5a44748d2219e2dcea731916a4d54ed19cf
David <dlapsley@cyb3r.us>
2011-09-12 12:04:03 -0400

    Added LSI MegaRAID firmware and drivers.

--- src/dimino6/bin/client.sh
+++ src/dimino6/bin/client.sh
@@ -5,9 +5,9 @@ ERL=/opt/local/bin/erl
 ROOT=/opt/src/mark6/src/dimino6
 CLIENT_DIR=${ROOT}/lib/client/ebin
 CLIENT_MODULE=client
-CLIENT_FUNC=start
+CLIENT_FUNC=start_server
 
 
 ${ERL} -pa ${CLIENT_DIR} \
-	-run ${CLIENT_MODULE} ${CLIENT_FUNC} \
+	-s ${CLIENT_MODULE} ${CLIENT_FUNC} false \
 	-run init stop -noshell
--- src/dimino6/lib/client/test/client_tests.erl
+++ src/dimino6/lib/client/test/client_tests.erl
@@ -11,13 +11,15 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("main.hrl").
 
+-export([server/2]).
+
 my_test_() ->
     [
      {timeout, ?TEST_TIMEOUT_MS, ?_test(basic())}
     ].
 
 start_server(Responses) ->
-    {ok, spawn(?MODULE, server, [?DEFAULT_LISTEN_PORT, Responses])}.
+    {ok, spawn(?MODULE, server, [?DEFAULT_PORT, Responses])}.
 
 server(Listen_port, Responses) ->
     {ok, Listen_socket} = gen_tcp:listen(Listen_port, ?TCP_OPTIONS),
@@ -30,22 +32,38 @@ wait_connect(Listen_socket, List) ->
 handle_request(Socket, [H|T]) ->
     case gen_tcp:recv(Socket, 0) of
 	{ok, _Command} ->
-	    io:format("R: ~p~n", [H]),
-	    Xml ="ok",
+	    error_logger:info_msg("R: ~p~n", [H]),
+	    Xml ="quit",
 	    ok = gen_tcp:send(Socket, Xml),
 	    handle_request(Socket, T);
-	Done ->
-	    error_logger:info_msg("recv done: ~p~n", [Done])
+	{error, Reason} ->
+	    error_logger:info_msg("R: done: ~p~n", [Reason])
     end;
 handle_request(Socket, []) ->
     ok.
 
+commands() ->
+    [
+     "input_stream=add:RDBE1:vdif:eth0:192.162.1.38;",
+     "input_stream=add:RDBE2:vdif:eth0:192.162.1.40;",
+     "record=on:076-1233:exp123:wf;",
+     "vol_stack?;",
+     "record=off",
+     "scan_info?;",
+     "scan_check?;"
+    ].
+
 % Start of tests...
 basic() ->
-    error_logger:info_msg("client_tests:basic()~n"),
+    error_logger:info_msg("client_tests:basic!()~n"),
     process_flag(trap_exit, true),
-    {ok, Pid} = start_server([]),
-    client:start(),
-    exit(Pid, "Finished test."),
+    {ok, Dummy_pid} = start_server([]),
+    timer:sleep(1000),
+    {ok, Client_pid} = client:start_server(true),
+    timer:sleep(1000),
+    lists:foreach(fun(X) -> Client_pid ! {self(), X} end, commands()),
+    timer:sleep(1000),
+    exit(Client_pid, "Shutting down client pid."),
+    exit(Dummy_pid, "Sutting down dummy pid."),
     ok.
 
