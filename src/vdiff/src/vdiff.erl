0831e0e81c69a062522ec2e7493f94afd2a1150e
David <del@haystack.mit.edu>
2011-09-22 19:31:24 -0400

    Correctly decoding (endian convesion and all).

--- src/vdiff/src/vdiff.erl
+++ src/vdiff/src/vdiff.erl
@@ -15,66 +15,63 @@
 
 %% API
 -export([start_server/1, server/1]).
+-define(FRAME_SIZE, 8224).
 
 trim_whitespace(Input) ->
     LS = re:replace(Input, "^[ \\t\\n]*", "", [{return, list}]),
     RS = re:replace(LS, "[ \\t\\n]*$", "", [{return, list}]),
     RS.
 
-start_server(Test) ->
-    {ok, spawn(?MODULE, server, [false])}.
+scan_file(Fd, Frame, {ok, Binary}) ->
+    io:format("~p~n", [ Frame ]),
+    decode_header(binary:part(Binary, {0, 32})),
+    scan_file(Fd, Frame + 1, file:read(Fd, ?FRAME_SIZE));
+scan_file(Fd, Frame, eof) ->
+	file:close(Fd);
+scan_file(Fd, Frame, {error, Reason}) ->
+	file:close(Fd),
+	{error, Reason}.
 
-server(Test) ->
+start_server(File_name) ->
+    {ok, spawn(?MODULE, server, [File_name])}.
+
+server(File_name) ->
     error_logger:info_msg("Server start."),
-    io:format(?HLINE ++ "\n"),
-    io:format("Mark6 CLI~n", []),
-    io:format("Copyright 2011 MIT Haystack Observatory~n", []),
-    io:format("del@haystack.mit.edu~n~n", []),
-    io:format("Type 'quit;' to exit CLI...~n", []),
-    io:format(?HLINE ++ "\n"),
-    case gen_tcp:connect(?DEFAULT_HOST, ?DEFAULT_PORT,
-			 ?TCP_OPTIONS) of
-    	{ok, Sock} ->
-	    io:format("Connected to server...~n"),
-	    loop(Sock, Test);
-	{error, Reason} ->
-	    error_logger:warn_msg("Error connecting to server: ~w~n",
-				  [ Reason ]);
-	_ ->
-	    error_logger:warn_msg("Unknown error connecting to server~n")
-    end.
+    error_logger:info_msg("Filename: ~p~n", [ File_name ]),
+    {ok, Fd} = file:open(File_name, [ read, binary ]),
+    FRAME_SIZE = 8224,
+    scan_file(Fd, 0, file:read(Fd, ?FRAME_SIZE)).
+    
+decode_header(<<
+		W0B0:8, W0B1:8, W0B2:8, W0B3:8, 
+		W1B0:8, W1B1:8, W1B2:8, W1B3:8, 
+		W2B0:8, W2B1:8, W2B2:8, W2B3:8, 
+		W3B0:8, W3B1:8, W3B2:8, W3B3:8, 
+		W4B0:8, W4B1:8, W4B2:8, W4B3:8, 
+		W5B0:8, W5B1:8, W5B2:8, W5B3:8, 
+		W6B0:8, W6B1:8, W6B2:8, W6B3:8, 
+		W7B0:8, 16#DE:8, 16#AD:8, 16#AB:8
+	      >>) ->    
+    <<I:1, L:1, Seconds_a:6>> = <<W0B3:8>>,
+    <<Epoch_seconds:32>> = <<Seconds_a, W0B2, W0B1, W0B0>>,
+    <<_:2,Reference_epoch:6>> = << W1B3 >>,
+    <<Data_frame:32>> = <<2#0:8, W1B2, W1B1, W1B0>>,
+    <<V:3, Log_channels:5>> = <<W2B3>>,
+    <<Length:32>> = <<2#0:8, W2B2, W2B1, W2B0>>,
+    <<C:1, Bits:5,Thread_id_a:2>> = <<W3B3>>,
+    <<Thread_id:32>> = <<2#0:22, Thread_id_a:2, W3B2:8>>,
+    <<Station_id:32>> = <<2#0:16, W3B1:8, W3B1:8>>,
+    <<EDV:8>> = <<W4B3>>,
 
-loop(Sock, Test) ->
-    % Input = io:get_line("mark6>"),
-    %error_logger:info_msg("~p~n", [Input]),
-    case io:get_line("mark6>") of
-	eof ->
-	    error_logger:info_msg("Done");
-	{error, Reason } ->
-	    error_logger:info_msg("Error parsing command: ~w~n",
-				  [ Reason ]);
-	Cmd ->
-	    Cleaned_cmd = string:to_lower(trim_whitespace(Cmd)),
-	    case string:equal(Cleaned_cmd, "quit;") of
-		true ->
-		    error_logger:info_msg("Quitting...");
-		false ->
-		    gen_tcp:send(Sock, Cleaned_cmd),
-		    Response = get_response(Sock, []),
-		    Response_lines = string:tokens(Response, "\n"),
-		    lists:map(fun(X) ->
-				      io:format("~s~n", [X])
-			      end, Response_lines),
-		    loop(Sock, Test)
-	    end	
-    end.
 
-get_response(Sock, List) ->
-    case gen_tcp:recv(Sock, 1) of
-	{ok, ";"} ->
-	    lists:concat(lists:reverse([ ";" | List]));
-	{ok, Char} ->
-	    get_response(Sock, [Char|List]);
-	Error ->
-	    error_logger:info_msg("R: ~p~n", [Error])
-    end.
+    io:format("MW          : ~.16B~.16B~.16B~.16B~n", [ 0, 0, 0, W7B0 ]),
+    io:format("Data_frame  : ~p~n~n", [ Data_frame ]).
+
+    % io:format("MW            : ~p ~p~n", [ MW1, MW2]),
+    % io:format("Data_frame  : ~.16B~.16B~.16B~n", [ W1B2, W1B1, W1B0 ]),
+    % io:format("Frame_length: ~p~n", [ Length ]),
+    % io:format("Epoch       : ~p~n", [ Reference_epoch ]),
+    % io:format("Epoch_seconds: ~.16B~.16B~.16B~.16B~n", [ W0B3, W0B2, W0B1, W0B0 ]),
+    % io:format("Epoch_seconds: ~p~n", [ Epoch_seconds ]).
+
+		
