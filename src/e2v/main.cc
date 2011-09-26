11e2923d533dee55b850b34dfe9bae57c9443355
David <del@haystack.mit.edu>
2011-09-26 18:05:36 -0400

    Added thread splitting based on UDP port.

--- src/e2v/main.cc
+++ src/e2v/main.cc
@@ -1,3 +1,5 @@
+// C includes.
+#include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
 #include <pcap.h>
@@ -8,66 +10,106 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+//C++ includes
+#include <list>
+#include <sstream>
+#include <string>
+
+// Framework includes.
+#include <boost/foreach.hpp>
+#include <boost/program_options.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/algorithm/string.hpp> 
+
+// Namespaces.
+namespace po = boost::program_options;
+
 #define ETHER_TYPE_IP (0x0800)
 #define ETHER_TYPE_8021Q (0x8100)
 #define UDP_PROTOCOL_NUMBER (17)
 #define UDP_HEADER_LENGTH (8)
 
+void usage() {
+  std::cout << "e2v <input file> <port list>" << std::endl;
+}
+
+// Only look at last two digits of port for demux.
+const int MAX_FDS = 100;
+char fds[MAX_FDS];
+
+std::list<int> PORT_LIST;
+
 int
 main(int argc, char *argv[]) {
-  char *dev = argv[1];
-  char *out = argv[2];
+  if (argc < 4) {
+    usage();
+    return(-1);
+  }
+
+  std::string dev(argv[1]);
+  std::string out(argv[2]);
+  for (int i=3; i<argc; i++) {
+    PORT_LIST.push_back(atoi((const char*)argv[i]));
+  }
+
+  BOOST_FOREACH(unsigned int port, PORT_LIST) {
+    std::cout << port << " ";
+    std::ostringstream oss (std::ostringstream::out);
+    oss << out << "-" << port << ".vdif";
+    std::cout << "Opening: " << oss.str() << std::endl;
+    fds[port % MAX_FDS] =  ::open(oss.str().c_str(), O_WRONLY
+				  | O_CREAT, S_IRWXU);
+  }
+
   struct pcap_pkthdr header;
   const u_char* packet;
 
   std::cout << "Dev: " << dev << std::endl;
   std::cout << "Out: " << out << std::endl;
 
-  int out_fd = ::open(out, O_WRONLY | O_CREAT, S_IRWXU);
-
   //open the pcap file 
   pcap_t *handle; 
-  char errbuf[PCAP_ERRBUF_SIZE]; //not sure what to do with this, oh well 
-  handle = pcap_open_offline(dev, errbuf);   //call pcap library function 
+  char errbuf[PCAP_ERRBUF_SIZE];
+  handle = pcap_open_offline(dev.c_str(), errbuf);
   if (handle == NULL) { 
     std::cout << "Couldn't open pcap file " << dev << " " << errbuf
 	      << std::endl;
     return(-1); 
   }
 
+  // Packet processing loop.
   while (packet = pcap_next(handle, &header)) {
     u_char *pkt_ptr = (u_char *)packet;
     int ether_type = ((int)(pkt_ptr[12]) << 8) | (int)pkt_ptr[13]; 
     int ether_offset = 0; 
-    if (ether_type == ETHER_TYPE_IP) //most common 
+    if (ether_type == ETHER_TYPE_IP)
       ether_offset = 14; 
-    else if (ether_type == ETHER_TYPE_8021Q) //my traces have this 
+    else if (ether_type == ETHER_TYPE_8021Q)
       ether_offset = 18; 
     else 
-      fprintf(stdout, "Unknown ethernet type, %04X, skipping...\n",
-	      ether_type); 
+      printf("Unknown ethernet type, %04X, skipping...\n", ether_type);
  
     //parse the IP header 
-    pkt_ptr += ether_offset;  //skip past the Ethernet II header 
-    struct ip *ip_hdr = (struct ip *)pkt_ptr; //point to an IP header structure    
-    int header_length = ip_hdr->ip_hl*4;
-    int packet_length = ntohs(ip_hdr->ip_len);
+    pkt_ptr += ether_offset;
+    struct ip *ip_hdr = (struct ip *)pkt_ptr;
+    const int header_length = ip_hdr->ip_hl*4;
+    const int packet_length = ntohs(ip_hdr->ip_len);
     if (ip_hdr->ip_p == UDP_PROTOCOL_NUMBER) {
-      // std::cout << "Great " << header_length
-      // << " " << packet_length << std::endl;
       pkt_ptr += header_length;
       struct udphdr* udp_hdr = (struct udphdr*)pkt_ptr;
-      int udp_length = ntohs(udp_hdr->len) - UDP_HEADER_LENGTH;
+      const int dport = ntohs(udp_hdr->dest);
+      const int udp_length = ntohs(udp_hdr->len) - UDP_HEADER_LENGTH;
       pkt_ptr += UDP_HEADER_LENGTH;
       u_char* data = pkt_ptr;
-      int nb = write(out_fd, data, udp_length);
-      // std::cout << nb << std::endl;
+      int nb = write(fds[dport%MAX_FDS], data, udp_length);
     }
-
   } //end internal loop for reading packets (all in one file) 
  
   pcap_close(handle);  //close the pcap file 
-  close(out_fd);
+
+  BOOST_FOREACH(unsigned int port, PORT_LIST) {
+    close(fds[port % MAX_FDS]);
+  }
   return(0);
 }
 
